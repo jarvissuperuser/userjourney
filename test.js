a8b411de3649a4dd035cb1bfd3d1d152fd9b706b
timothy <mugadzatt001@gmail.com>
2018-04-12 16:01:24 +0200

    tests

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ var uj = new UjC(undefined,"./test.db");
 
 uj.pivotImg = "./test1.png";
 uj.testImg = "./test2.png";
-uj.testLocations = process.argv.splice(2)[0];
+uj.testLocations = process.argv.splice(2)[0]?process.argv.splice(2)[0]:"http://google.co.za";
 uj.filesExist["pivot"] = true;
 var test = new Promise(uj.checkFilesP);
-uj.fileName = uj.pivotImg;
+uj.fileName = uj.testImg;
 test.then(()=>{return new Promise(uj.getScreensP)})
 .catch(function(e){
     console.log(e);
