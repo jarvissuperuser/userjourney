b523cd0a0cf83b0eea200a322cc1b51f54e604ae
timothy <mugadzatt001@gmail.com>
2018-04-16 12:19:36 +0200

    re commit

--- test.js
+++ test.js
@@ -10,23 +10,23 @@ var moment = require("moment");
 var uj = new UjC(undefined,"./test.db");
 
 uj.timestamp = moment().format("MM-D-YY-h-mm-s");
-uj.project = "test";
-// uj.pivotImg = "./test.png";
-// uj.testImg = "./test2.png";
-
-
-
+if (process.argv.splice(2)[0]==="-t"){
+ uj.pivotImg = "./test.png";
+ uj.testImg = "./test2.png";
+}
 uj.testLocations = process.argv.splice(2)[0]?process.argv.splice(2)[0]:"http://timeslive.co.za";
 //uj.filesExist["pivot"] = true;
 var test = new Promise(function(w,l){
-    uj.setup();
+    
     uj.timestamp = moment().format("MM-D-YY-h-mm-s");
+    uj.project = "test";
+    uj.setup();
     uj.checkFilesP(w,l);
 });
 
 test.then(()=>{
     //uj.filesInit();
-    uj.setup();
+    //uj.setup();
     uj.filesExist['test'] = true;
     return new Promise(uj.getScreensP)})
 .then(function(){
